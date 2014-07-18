804ba031eb37e11823730f0754ad82fe23578129
Ryoji Osawa <ryojiosawa@gmail.com>
2014-07-18 15:46:04 -0700

    Make "setup" task update "self" remote site setting.  Also, refactor "setup" and "show" tasks using async module.

--- tasks/cinnamon.js
+++ tasks/cinnamon.js
@@ -6,7 +6,7 @@ var jsforce = require('jsforce'),
     _ = require('underscore'),
     async = require('async'),
     CLIENT_ID = '__4l3n4c',
-    CINNAMON_REST_BASEURL = '/services/apexrest/cinnamon',
+    CINNAMON_REST_BASE_URL = '/services/apexrest/cinnamon',
     SELF_REMOTE_SITE = 'cinnamon__self';
 
 module.exports = function (grunt) {
@@ -14,120 +14,105 @@ module.exports = function (grunt) {
     var config = grunt.file.readJSON('./cinnamon.json');
 
     grunt.registerTask('setup', function () {
-        var done = this.async();
+        var conn = new jsforce.Connection(),
+            done = this.async();
 
-        var conn = new jsforce.Connection();
+        async.series({
+            connect: function (callback) {
+                conn.login(config.username, config.password, function (err, userInfo) {
+                    if (err) {
+                        callback(err);
+                        done(err);
+                    }
+                    callback(null);
+                });
+            },
+            cinnamonSettings: function (callback) {
+                var data = {
+                    request: {
+                        proxyUrl: config.seleniumProxyURL,
+                        proxyPort: Number(config.seleniumProxyPort),
+                        proxyUsername: config.sauceUsername,
+                        proxyToken: config.sauceAccessKey,
+                        testPrefix: config.testPrefix,
+                        orgUnderTestUsername: config.username,
+                        orgUnderTestLoginUrl: conn.instanceUrl
+                    },
+                    clientId: CLIENT_ID
+                };
 
-        conn.login(config.username, config.password, function (err, userInfo) {
-            if (err) done(err);
+                conn.apex.post(CINNAMON_REST_BASE_URL + '/config', data, function (err, res) {
+                    if (err) {
+                        callback(err);
+                        done(err);
+                    }
 
-            var data = {
-                request: {
-                    proxyUrl: config.seleniumProxyURL,
-                    proxyPort: Number(config.seleniumProxyPort),
-                    proxyUsername: config.sauceUsername,
-                    proxyToken: config.sauceAccessKey,
-                    testPrefix: config.testPrefix,
-                    orgUnderTestUsername: config.username,
-                    orgUnderTestLoginUrl: conn.instanceUrl
-                },
-                clientId: CLIENT_ID
-            };
+                    grunt.log.writeln("Cinnamon settings updated");
+                    callback(null);
+                });
+            },
+            remoteSiteSettings: function (callback) {
+                var data = {
+                    currentName: 'cinnamon__self',
+                    metadata: {
+                        fullName: 'cinnamon__self',
+                        isActive: true,
+                        url: conn.instanceUrl
+                    }
+                };
 
-            conn.apex.post(CINNAMON_REST_BASEURL + '/config', data, function (err, res) {
-                if (err) done(err);
-
-                grunt.log.writeln("Cinnamon settings updated.");
-                done();
-            });
-
-            // TODO: update remote site setting - self
+                conn.metadata.updateAsync('RemoteSiteSetting', data).complete(function (err, res) {
+                    if (err) {
+                        callback(err);
+                        done(err);
+                    }
+                    grunt.log.writeln("Remote site settings updated");
+                    callback(null);
+                });
+            }
+        }, function (err, results) {
+            grunt.log.writeln("Setup completed successfully");
+            done();
         });
     });
 
     grunt.registerTask('show', function () {
-        var done = this.async(),
+        var conn = new jsforce.Connection(),
+            done = this.async(),
             tests;
 
-        var conn = new jsforce.Connection();
-
-        conn.login(config.username, config.password, function (err, userInfo) {
-            if (err) done(err);
-
-            async.series([
-                function(callback) {
-
-                },
-                function(callback) {
-                    // update "self" remote site setting via metadata API
-                    conn.metadata.read('RemoteSiteSetting', [SELF_REMOTE_SITE], function (err, metadata) {
-                        console.log(metadata);
-                    });
-
-                    var data = {
-                        currentName: SELF_REMOTE_SITE,
-                        metadata: {
-                            fullName: SELF_REMOTE_SITE,
-                            isActive: true,
-                            url: conn.instanceUrl
-                        }
+        async.series({
+            connect: function (callback) {
+                conn.login(config.username, config.password, function (err, userInfo) {
+                    if (err) {
+                        callback(err);
+                        done(err);
                     }
-                    conn.metadata.updateAsync('RemoteSiteSetting', data).complete(function(err, res) {
-                        if (err) done(err);
-                    });
-                }
-            ], function(err, results) {
-                if (err) done(err);
+                    callback(null);
+                });
+            },
+            queryTests: function (callback) {
+                conn.apex.get(CINNAMON_REST_BASE_URL + '/tests/definition', function (err, res) {
+                    if (err) {
+                        callback(err);
+                        done(err);
+                    }
 
-                grunt.log.writeln('Cinnamon setup completed.');
-            });
+                    grunt.log.writeln('Number of Tests:' + res.length);
+                    _.chain(res)
+                        .pluck('Name')
+                        .sortBy(function (a, b) {
+                            if (a > b) return 1;
+                            if (a < b) return -1;
+                            return 0;
+                        })
+                        .each(function (test) {
+                            grunt.log.writeln(test);
+                        });
 
-            conn.apex.get(CINNAMON_REST_BASEURL + '/tests/definition', function (err, res) {
-                if (err) done(err);
-
-                grunt.log.writeln('Number of Tests:' + res.length);
-                _.chain(res)
-                    .pluck('Name')
-                    .sortBy(function (a, b) {
-                        if (a > b) return 1;
-                        if (a < b) return -1;
-                        return 0;
-                    })
-                    .each(function (test) {
-                        grunt.log.writeln(test);
-                    });
-
-                done();
-            });
-        });
-    });
-
-    grunt.registerTask('meta', function () {
-        var done = this.async();
-
-        var conn = new jsforce.Connection();
-
-        conn.login(config.username, config.password, function (err, userInfo) {
-            if (err) done(err);
-
-            conn.metadata.read('RemoteSiteSetting', ['cinnamon__self'], function (err, metadata) {
-                console.log(metadata);
-            });
-
-            var data = {
-                currentName: 'cinnamon__self',
-                metadata: {
-                    fullName: 'cinnamon__self',
-                    isActive: true,
-                    url: 'https://na15.salesforce.com'
-                }
+                    callback(null);
+                });
             }
-            conn.metadata.updateAsync('RemoteSiteSetting', data).complete(function(err, res) {
-                if (err) done(err);
-
-                grunt.log.writeln(res);
-                done();
-            });
         });
     });
 };
